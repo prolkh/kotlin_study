93eb4f6da642e995d3df182a75939cb50d3b4efb
dongmahae <dongmahae@gmail.com>
2021-03-20 14:44:16 +0900

    kotlin study start

---
+++ src/main/kotlin/com/dongmahae/kotlinstudy/KotlinstudyApplication.kt
@@ -0,0 +1,11 @@
+package com.dongmahae.kotlinstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinstudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinstudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/dongmahae/kotlinstudy/KotlinstudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dongmahae.kotlinstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinstudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
